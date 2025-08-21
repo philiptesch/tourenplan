6e177f094cbf6ebf598df87fadba9124953b3fe5
Philip Tesch <philiptesch99@gmail.com>
2025-08-21 23:54:11 +0200

    feat: add tour table with hours and trucks

--- src/app/main-page/tour-order/tour-order.component.ts
+++ src/app/main-page/tour-order/tour-order.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Timeslot } from '../../services/timeslot_model';
+
 
 @Component({
   selector: 'app-tour-order',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './tour-order.component.html',
   styleUrl: './tour-order.component.scss'
 })
 export class TourOrderComponent {
-
+hours = [8, 9, 10, 11, 12, 13, 14, 15, 16];
+lkws = [1, 2, 3, 4, 5, 6, 7, 8];
 }
---
+++ src/app/services/lkw_model.ts
@@ -0,0 +1,4 @@
+export interface Slot {
+  start: string;      
+  end: string;        
+}
