308e620b1a9ec0ae43148f48660f5444df94c124
Philip Tesch <philiptesch99@gmail.com>
2025-08-27 23:12:21 +0200

    feat: Beginn with FormsValidation new Tour-package

--- src/app/main-page/tour-order/tour-package/add-new-package/add-new-package.component.ts
+++ src/app/main-page/tour-order/tour-package/add-new-package/add-new-package.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-add-new-package',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './add-new-package.component.html',
   styleUrl: './add-new-package.component.scss'
 })
 export class AddNewPackageComponent {
+time!: string
+tourcode!: string;
+article!: string
+lkws = [1, 2, 3, 4, 5, 6, 7, 8,9,10,11,12,13,14,16,17,18,19,20];
+  newTour = {
+    time: '',
+    tourcode: '',
+    article: ''
+  };
+
+createTour(event: Event) {
+  event.preventDefault();
+    console.log('Neue Tour:', {
+      time: this.time,
+      tourcode: this.tourcode,
+      article: this.article
+    });
+
+}
 
 }
