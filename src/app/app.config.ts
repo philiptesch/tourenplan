c5733d3083826ab487669b670d7101b0a47f0dfb
Philip Tesch <philiptesch99@gmail.com>
2025-08-29 16:48:09 +0200

    feat: install firebase for realt time data

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,10 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync()]
+  providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync(), provideFirebaseApp(() => initializeApp({"projectId":"tourenplan-6d4ae","appId":"1:619354557814:web:3f6b83b6d088ebeb5a3e57","storageBucket":"tourenplan-6d4ae.firebasestorage.app","apiKey":"AIzaSyAfOaj-ihalZHR_ZavxOvvmo0Swe1WEnxs","authDomain":"tourenplan-6d4ae.firebaseapp.com","messagingSenderId":"619354557814"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())]
 };
