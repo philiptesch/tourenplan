26f8c62cd12da5bc594c4469b6fbe187e4ed0ce3
Philip Tesch <philiptesch99@gmail.com>
2025-09-21 23:22:50 +0200

    feat: add article view designs mat card and scroll-bar

--- src/app/main-page/tour-order/package/article-view/article-view.component.ts
+++ src/app/main-page/tour-order/package/article-view/article-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
@@ -8,17 +8,30 @@ import { Tour } from '../../../../interfaces/tour.interface';
 @Component({
   selector: 'app-article-view',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './article-view.component.html',
   styleUrl: './article-view.component.scss'
 })
-export class ArticleViewComponent {
+export class ArticleViewComponent implements OnInit {
 tourId!: string
 article!: Article[] 
+newTourId!:string
 
   constructor(public dialogRef: MatDialogRef<ArticleViewComponent>, @Inject(MAT_DIALOG_DATA) public data: { article: Article[], tourId: string, tour: Tour[]  }) {
     this.tourId = data.tourId
     this.article = data.article
+  }
+
+
+  ngOnInit(): void {
+    this.changeTourName()
+    
+  }
+
+
+  changeTourName() {
+    this.newTourId = this.tourId.split('drop-')[1]
+    
   }
   
 }
