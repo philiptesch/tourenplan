8e1817b967097d03e45b9dcdd694a192d7ad98a2
Philip Tesch <philiptesch99@gmail.com>
2025-10-14 20:28:53 +0200

    feat: begin with design login add router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { MainPageComponent } from './main-page/main-page.component';
+import { AuthComponent } from './auth/auth.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+export const routes: Routes = [
+     {
+    path: 'MainPage',
+    component: MainPageComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'signUp',
+    component: RegisterComponent,
+  },
 
-export const routes: Routes = [];
+];
