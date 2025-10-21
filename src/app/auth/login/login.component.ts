76ecaa8d1b3754260f6455a0c232764d10c175f7
Philip <philiptesch99@gmail.com>
2025-10-21 19:46:47 +0200

    feat: styling login window

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-
+import {MatIconModule} from '@angular/material/icon';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
