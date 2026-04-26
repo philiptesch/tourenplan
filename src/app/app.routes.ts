2c1a82e01b62ef0b647ed781e72ab943d24e4369
Philip <philiptesch99@gmail.com>
2026-04-26 14:20:50 +0200

    add new logo component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,14 +2,14 @@ import { Routes } from '@angular/router';
 import { MainPageComponent } from './main-page/main-page.component';
 import { AuthComponent } from './auth/auth.component';
 import { LoginComponent } from './auth/login/login.component';
+import { LogoPageComponent } from './auth-component/logo-page/logo-page.component';
 import { RegisterComponent } from './auth/register/register.component';
 export const routes: Routes = [
 
    {
-    path: 'login',
-    component: LoginComponent,
+    path: 'logo',
+    component: LogoPageComponent,
   },
-   { path: '', redirectTo: '/login', pathMatch: 'full' },
 
      {
     path: 'mainPage',
---
+++ src/app/auth-component/logo-page/logo-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logo-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './logo-page.component.html',
+  styleUrl: './logo-page.component.scss'
+})
+export class LogoPageComponent {
+
+}
