81da8a49b8169889aabe4caf9f20bd13cf81f223
Philip <philiptesch99@gmail.com>
2026-04-26 22:32:22 +0200

    add animation logo first view

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,21 +1,32 @@
 import { Component, inject } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import { Router } from '@angular/router';
-import {LogoPageComponent} from from './logo-page/logo-page.component';
-
+import { LogoPageComponent } from './logo-page/logo-page.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule, LogoPageComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
    private router = inject(Router);
+   changeHeightAndWidth: boolean = false
 
-  constructor() { }
+  constructor() {
+      this.startLogoAnimation()
 
+   }
+
+
+
+   startLogoAnimation() {
+    setTimeout(() => {
+      this.changeHeightAndWidth = true
+    }, 2000);
+
+   }
 
 
   goToSignUp() { 
--- src/app/auth/login/logo-page/logo-page.component.ts
+++ src/app/auth/login/logo-page/logo-page.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-logo-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './logo-page.component.html',
   styleUrl: './logo-page.component.scss'
 })
 export class LogoPageComponent {
+
+
+     changeHeightAndWidth: boolean = false
+
+  constructor() {
+      this.startLogoAnimation()
+
+   }
+
+
+
+   startLogoAnimation() {
+    setTimeout(() => {
+      this.changeHeightAndWidth = true
+    }, 3000);
+
+   }
 
 }
